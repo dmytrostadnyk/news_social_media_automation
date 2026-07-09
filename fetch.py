459a6f766ff6fa01827b7b9157ad720a0762f24d
Dmytro Stadnyk <kh22062017@gmail.com>
2026-07-09 13:41:58 +0300

    initial article fetching functions added

--- fetch.py
+++ fetch.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import feedparser
+from trafilatura import fetch_url, extract
 
 
 # Fetching an image from Pexels based on a query created by an LLM using the Pexels API
@@ -16,3 +18,15 @@ def fetch_image_from_pexels(image_query: str) -> str:
     if not photos:
         raise ValueError(f"No images found for query: {image_query}")
     return photos[0]["src"]["large"]
+
+# Fetching an article link from RSS feed using feedparser
+def fetch_article_from_rss() -> str:
+    feed = feedparser.parse("https://feeds.bbci.co.uk/news/business/rss.xml")
+    entry = feed.entries[0]
+    return entry.link
+
+# Fetching an article text from a URL extracted from RSS feed
+def fetch_article_text() -> str:
+    article_url = fetch_url(entry.link)
+    article = extract(article_url, with_metadata=False, include_comments=False)
+    return article
